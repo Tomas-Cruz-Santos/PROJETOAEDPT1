948fa3d9ecb379d7db9147c78e3e3510ab2582af
Tomas Santos <a22406019@alunos.ulht.pt>
2026-03-29 12:38:22 +0100

    input invalido(era facil)
    agora preciso de perceber isso dos ficheiros para fazer o main

--- src/pt/ulusofona/aed/deisiworldmeter/Input_Invalido.java
+++ src/pt/ulusofona/aed/deisiworldmeter/Input_Invalido.java
@@ -1,4 +1,33 @@
 package pt.ulusofona.aed.deisiworldmeter;
 
 public class Input_Invalido {
+    String nomeFicheiro;
+    int linhasCorretas;
+    int linhasIncorretas;
+    int primeiraLinhaIncorreta;
+
+
+    public Input_Invalido(String file) {
+        this.nomeFicheiro = file;
+        this.linhasCorretas = 0;
+        this.linhasIncorretas = 0;
+        this.primeiraLinhaIncorreta = -1;
+    }
+
+    public void contalinhascorretas() {
+        linhasCorretas++;
+    }
+
+    public void contalinhasIncorretas(int nlinha) {
+        if (linhasIncorretas == 0) {
+            primeiraLinhaIncorreta = nlinha;
+        }
+        linhasIncorretas++;
+    }
+
+    @Override
+    public String toString() {
+        return nomeFicheiro + " | " + linhasCorretas + " | " + linhasIncorretas + " | " + (primeiraLinhaIncorreta != -1 ? primeiraLinhaIncorreta : "-1");
+    }
 }
+
